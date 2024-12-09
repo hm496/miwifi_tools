45258464538d428693c9bc4fb89ea80334596e9c
wangchong23 <wangchong23@meituan.com>
2024-12-09 15:53:32 +0800

    feat: 修复脚本

--- ssh.js
+++ ssh.js
@@ -22,7 +22,7 @@ async function request_smartcontroller(stok, command) {
     },
     body: `payload=` + encodeURIComponent(`{"command":"scene_setting","name":"'$(${command})'","action_list":[{"thirdParty":"xmrouter","delay":17,"type":"wan_block","payload":{"command":"wan_block","mac":"00:00:00:00:00:00"}}],"launch":{"timer":{"time":"3:${timeCount}","repeat":"0","enabled":true}}}`),
   }))
-    .then((response) => response.text())
+    .then((response) => response.json())
     .then((text) => console.log(text));
 
   await fetch(new Request(location.origin + path, {
@@ -32,7 +32,7 @@ async function request_smartcontroller(stok, command) {
     },
     body: `payload=` + encodeURIComponent(`{"command":"scene_start_by_crontab","time":"3:${timeCount}","week":0}`),
   }))
-    .then((response) => response.text())
+    .then((response) => response.json())
     .then((text) => console.log(text));
 
   window.timeCount = window.timeCount + 1;
@@ -42,7 +42,7 @@ async function set_sys_time(stok) {
   let path = `/cgi-bin/luci/;stok=${stok}/api/misystem/set_sys_time?time=2023-2-19%2023:4:47&timezone=CST-8`;
   console.log(path);
   await fetch(new Request(location.origin + path))
-    .then((response) => response.text())
+    .then((response) => response.json())
     .then((text) => console.log(text));
 }
 
@@ -56,10 +56,15 @@ async function enableSSH() {
 
   await set_sys_time(stok);
 
+  console.log('0/4: nvram set ssh_en=1 && nvram commit');
   await request_smartcontroller(stok, `nvram set ssh_en=1 && nvram commit`);
+  console.log('1/4: sed -i s/release/debug/g /etc/init.d/dropbear');
   await request_smartcontroller(stok, `sed -i s/release/debug/g /etc/init.d/dropbear`);
+  console.log('2/4: /etc/init.d/dropbear enable');
   await request_smartcontroller(stok, `/etc/init.d/dropbear enable`);
+  console.log('3/4: /etc/init.d/dropbear restart');
   await request_smartcontroller(stok, `/etc/init.d/dropbear restart`);
+  console.log('4/4: done');
 }
 
 enableSSH();
